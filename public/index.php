67129c62dc242f7efe17b9cefbb2af1d46e1dec6
a.pupyishev <a.pupyishev@robo.cash>
2022-04-29 23:51:02 +0700

    send answer from bot

--- public/index.php
+++ public/index.php
@@ -4,4 +4,23 @@ require "../vendor/autoload.php";
 
 $data = json_decode(file_get_contents('php://input'), true);
 
+$message = $data['message'];
 error_log(json_encode($data));
+
+sendAnswer('sendMessage', ['text' => 'Вот мой ответ!']);
+
+function sendAnswer($method, $data, $headers = [])
+{
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://api.telegram.org/bot5393296749:AAFNrpq4TTDH9tFmuKVX0FnWvarnKlb7qh0/' . $method,
+        CURLOPT_POSTFIELDS => json_encode($data),
+        CURLOPT_HTTPHEADER => array_merge(['Content-Type: application/json'], $headers)
+    ]);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($result, 1) ?? $result;
+}
